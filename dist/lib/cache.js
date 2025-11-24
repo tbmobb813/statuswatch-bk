1fb67dadfac7ade7ffeff129d2c5f5c9c9f4ebfb
tbmobb813 <jsn.nix@gmail.com>
2025-11-24 00:17:50 -0500

    change to cache

--- dist/lib/cache.js
+++ dist/lib/cache.js
@@ -2,6 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.cacheSet = cacheSet;
 exports.cacheGet = cacheGet;
@@ -9,9 +10,21 @@ exports.cacheHas = cacheHas;
 exports.cacheDel = cacheDel;
 exports.cacheClear = cacheClear;
 const lru_cache_1 = __importDefault(require("lru-cache"));
+let LRUCacheClass;
+try {
+    // Use require at runtime to handle different module systems (CJS vs ESM) in test runners
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const mod = require('lru-cache');
+    // module may export { LRUCache } or default or be the constructor itself
+    LRUCacheClass = (mod && ((_b = (_a = mod.LRUCache) !== null && _a !== void 0 ? _a : mod.default) !== null && _b !== void 0 ? _b : mod));
+}
+catch {
+    // Fallback to the imported LRU (should work in ESM environments)
+    LRUCacheClass = lru_cache_1.default;
+}
 const DEFAULT_MAX = 500;
 const DEFAULT_TTL = 1000 * 60 * 5; // 5 minutes
-const cache = new lru_cache_1.default({ max: DEFAULT_MAX, ttl: DEFAULT_TTL });
+const cache = new LRUCacheClass({ max: DEFAULT_MAX, ttl: DEFAULT_TTL });
 function cacheSet(key, value, ttlMs) {
     if (ttlMs) {
         cache.set(key, value, { ttl: ttlMs });
